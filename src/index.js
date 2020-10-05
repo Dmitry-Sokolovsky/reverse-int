a0afa12c5f26d826f8457e77c770cb2614dbb735
Dmitry-Sokolovsky <dmitry635494@gmail.com>
2020-10-05 20:20:08 +0300

    float: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n);
+    let result = 0;
+    for(; n; n = Math.floor(n / 10)) {
+        result *= 10;
+        result += n % 10;
+    }
+    return result;
+};
